d4d1e178d46fc4c53ce05fc93c4f1d6208a7ae1d
Yann Thoma <yann.thoma@heig-vd.ch>
2026-05-04 13:49:21 +0200

    Updating doc generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@ author = 'Tucuxi dev team'
 
 import os
 import sys
-sys.path.append(os.path.relpath("_ext"))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '_ext')))
 
 extensions = [
     'sphinx.ext.todo',
