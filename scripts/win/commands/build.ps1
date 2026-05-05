06d9b340fe4ea215c9eb22ac7df14dfb580665cf
Yann Thoma <yann.thoma@heig-vd.ch>
2026-05-05 16:17:53 +0200

    Adding build.ps1

---
+++ scripts/win/commands/build.ps1
@@ -0,0 +1,14 @@
+# Build the main project.
+
+$buildType = cmake_build_type
+$nproc = portable_nproc
+
+Write-Host "==> Building main project ($script:CONFIG)"
+cmake -S $script:REPO_ROOT -B $script:MAIN_BUILD_DIR `
+    -G Ninja `
+    -DCMAKE_BUILD_TYPE=$buildType `
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
+cmake --build $script:MAIN_BUILD_DIR -j $nproc
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
